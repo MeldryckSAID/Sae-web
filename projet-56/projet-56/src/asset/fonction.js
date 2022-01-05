fe5d104abdf3e1200adcf8923a99722805e2bb96
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-01-05 03:00:54 +0100

    carrousel essai footer et header autre page

--- projet-56/projet-56/src/asset/fonction.js
+++ projet-56/projet-56/src/asset/fonction.js
@@ -9,3 +9,38 @@ function bascule(cla) {
         }
     });
 }
+
+function carrousel3Images() {
+    var fi_lm = document.querySelectorAll('.fi_lm');
+    let d = 2500;
+    let delta = 1000;
+
+    fi_lm.forEach(function(img,indice) {
+        img.style.zIndex = fi_lm.length - indice;
+    });
+
+    let anim153 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim153.add({
+        targets: fi_lm,
+        translateX: [{value: '180', duration: d},
+                     {value: '-180', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (2*delta)+d;
+                         if (ind == 1) return delta;
+                         return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 2) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            return (3*delta)+(1*d);
+        }
+    });
+}
